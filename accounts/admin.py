c30982f1d3eea6eda070ca2b80435400d8d14d45
Hassan.TLK <hassan.tlk.85@gmail.com>
2024-01-16 19:53:11 +0330

    add App 'pages', add crispy form, logout not working

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
+from dataclasses import fields
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserCreationForm
+    model = CustomUser
+
+    # اضافه کردن فیلدهای جدید به صفحه ی مشاهده یوزرها
+    # در صفحه admin
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("age", )}),
+    )
+    # اضافه کردن فیلدهای جدید به هنگام اد کردن یوزر جدید
+    # در صفحه admin
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("age", )}),
+    )
+    #-------------------------------
+    list_display = ["username", "email", "age", "is_staff"]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "crispy_forms",
+    "crispy_bootstrap4",
     'accounts',
 ]
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath("templates"))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
